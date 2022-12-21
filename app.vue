94440ffeb74ccad3c225a5d9e6d973015497112a
Maxime Coriton <maxime.coriton@gmail.com>
2022-12-21 10:39:36 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="user" />
+    <Auth v-else />
+  </div>
+</template>
+
+<script setup>
+const user = useSupabaseUser();
+/* window.global = window;
+var exports = {}; */
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/supabase'],
+   /*  plugins: ['~/plugins/Amplify.ts'], */
+})
